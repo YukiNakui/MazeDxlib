5a6bfb311b9207f78b2d0d1c137791c9aa98e3f1
悠喜 名久井 <230227@jc-21.jp>
2025-02-25 12:43:49 +0900

    ステージのループを作れた

--- Enemy.h
+++ Enemy.h
@@ -2,20 +2,6 @@
 #include "./Library/GameObject.h"
 #include "./globals.h"
 #include "./Stage.h"
-#include <queue>
-#include <vector>
-#include <cmath>
-
-struct Node {
-    int x, y;
-    float gCost; // 移動コスト
-    float hCost; // ヒューリスティックコスト
-    float fCost() const { return gCost + hCost; } // 総コスト
-
-    bool operator>(const Node& other) const {
-        return fCost() > other.fCost();
-    }
-};
 
 class Enemy :
     public GameObject
@@ -37,11 +23,5 @@ public:
     void YCloserMove();
     void XYCloserMove();
     void XYCloserMoveRandom();
-    void AStarMove();
-    // ヒューリスティック関数 (プレイヤーとの距離を計算)
-    float Heuristic(int x1, int y1, int x2, int y2) {
-        return std::abs(x1 - x2) + std::abs(y1 - y2); // マンハッタン距離
-    }
-    void RetracePath(std::vector<std::vector<Node>>& cameFrom, Node current);
 };
 
